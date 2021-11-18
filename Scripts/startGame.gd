a5655d6a272964cf78caad5d978527dcdec5e6ba
Damian Pietryka <damianpietryka2003@gmail.com>
2021-11-18 10:57:52 +0100

    zmieniono nazwę do spawnowania kulki

--- Scripts/startGame.gd
+++ Scripts/startGame.gd
@@ -14,10 +14,10 @@ func _unhandled_input(event):
 		return
 	if event is InputEventMouseButton and event.is_pressed():
 		if event.button_index == BUTTON_LEFT:
-			spawn(get_global_mouse_position())
+			spawn_ball(get_global_mouse_position())
 
 
-func spawn(spawn_global_position):
+func spawn_ball(spawn_global_position):
 	var instance = ball_scene.instance()
 	instance.global_position = spawn_global_position
 	add_child(instance)
