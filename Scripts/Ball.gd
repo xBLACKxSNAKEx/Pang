ab74e2a8ffe35470d3f2b067a25eb582db0ba4fc
Damian Pietryka <damianpietryka2003@gmail.com>
2021-11-18 08:54:14 +0100

    Funkcja do "rozbijania" kulki na dwie

---
+++ Scripts/Ball.gd
@@ -0,0 +1,18 @@
+extends RigidBody2D
+
+var d = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+var i = 0
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func split():
+	d-=1
+	var dup = duplicate()
+	get_parent().add_child(dup)
+	pass
