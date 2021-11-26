3aacb1252b0be3786e73afcf6398c2f02f039257
Damian Pietryka <damianpietryka2003@gmail.com>
2021-11-26 20:03:19 +0100

    Maska kolizji dla gracza i kulek

--- Scripts/Ball.gd
+++ Scripts/Ball.gd
@@ -4,14 +4,20 @@ var d = 1
 
 onready var start_time = OS.get_ticks_msec()
 
+var A = 500
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 var i = 0
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+func _physics_process(delta):
+	var v = sqrt(linear_velocity.x*linear_velocity.x + linear_velocity.y*linear_velocity.y)
+	if v > A:
+		var normalized = Vector2(linear_velocity.x / v, linear_velocity.y/v)
+		apply_central_impulse(-normalized*10)
+	pass
 
 func split():
 	d-=1
